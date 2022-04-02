23355971ef405f083d3f894f475628075a94a16b
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-04-02 12:10:30 +0600

    home color added

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,13 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-cyan-800 to-rose-500 py-2">
       <Head>
         <title>NFT Drop</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      
+      <main className=""></main>
     </div>
   )
 }
